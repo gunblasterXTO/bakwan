e82341c415c06ba3d1aad0f5285bf69ae8560aa9
gunblasterXTO <naufal.abdila@outlook.com>
2024-04-16 14:40:38 +0700

    refactor: use scaffold instead of stack

--- lib/modules/splash/views/splash.dart
+++ lib/modules/splash/views/splash.dart
@@ -1,26 +1,37 @@
 import 'package:bakwan/common/style/ui.dart';
+import 'package:bakwan/routes/pages.dart';
 import 'package:flutter/material.dart';
+import 'package:get/route_manager.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Splashscreen extends StatelessWidget {
   const Splashscreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Container(color: UIConst.primaryColor),
-        Center(
-          child: Container(
-            width: 200,
-            height: 200,
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage("assets/images/bakwan.png"),
+    Future.delayed(const Duration(seconds: 1), () => Get.offNamed(Routes.home));
+
+    return Scaffold(
+      backgroundColor: UIConst.primaryColor,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              "assets/images/bakwan.png",
+              width: 200,
+              height: 200,
+            ),
+            Text(
+              "Bakwan",
+              style: GoogleFonts.raleway(
+                color: UIConst.secondaryColor,
+                fontSize: 24,
               ),
             ),
-          ),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
