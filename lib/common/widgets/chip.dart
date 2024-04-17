9bf52d2b5e4e4e22827dbf7d332f84959606a7c1
gunblasterXTO <naufal.abdila@outlook.com>
2024-04-17 09:11:38 +0700

    feat: create custom chip

---
+++ lib/common/widgets/chip.dart
@@ -0,0 +1,32 @@
+import 'package:bakwan/common/style/ui.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class CustomChip extends StatelessWidget {
+  final String text;
+  final Color color;
+  const CustomChip({
+    super.key,
+    required this.text,
+    required this.color,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: UIConst.standardPadding * 0.5),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(
+          UIConst.standardRad * 0.5,
+        ),
+        color: color,
+      ),
+      child: Text(
+        text,
+        style: context.textTheme.bodyMedium!.copyWith(
+          color: UIConst.white,
+        ),
+      ),
+    );
+  }
+}
