8795040b73b13b8059a4f505d313ed407622218a
gunblasterXTO <naufal.abdila@outlook.com>
2024-04-17 09:41:47 +0700

    feat: add more on common style and ui

--- lib/common/style/theme.dart
+++ lib/common/style/theme.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ThemeClass {
   static ThemeData lightTheme = ThemeData(
     useMaterial3: true,
     scaffoldBackgroundColor: Colors.white,
+    textTheme: GoogleFonts.ralewayTextTheme(),
   );
 }
--- lib/common/style/ui.dart
+++ lib/common/style/ui.dart
@@ -7,4 +7,11 @@ class UIConst {
   static Color primaryColor = const Color.fromRGBO(0, 71, 27, 1.0);
   static Color secondaryColor = const Color.fromRGBO(240, 235, 219, 1.0);
   static Color white = Colors.white;
+
+  // Config
+  static double appBarHeight = 64.0;
+  static double standardPadding = 8.0;
+  static double standardGapHeight = 8.0;
+  static double standardRad = 8.0;
+  static double standardSpacing = 4.0;
 }
