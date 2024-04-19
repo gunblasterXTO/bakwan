756657f2dc9e33c3030cd72f57d435bacf28ab5a
gunblasterXTO <naufal.abdila@outlook.com>
2024-04-20 06:41:50 +0700

    feat: routing to new activity page

--- lib/routes/pages.dart
+++ lib/routes/pages.dart
@@ -1,4 +1,5 @@
 import 'package:bakwan/modules/journal/views/journal.dart';
+import 'package:bakwan/modules/journal/views/new_activity.dart';
 import 'package:bakwan/modules/splash/views/splash.dart';
 import 'package:get/route_manager.dart';
 
@@ -12,5 +13,6 @@ class AppPages {
   static final routes = [
     GetPage(name: Routes.index, page: () => const Splashscreen()),
     GetPage(name: Routes.journal, page: () => const JournalPage()),
+    GetPage(name: Routes.newActivity, page: () => const NewActivityPage()),
   ];
 }
--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -5,4 +5,5 @@ abstract class Routes {
 
   static const index = "/";
   static const journal = "/journal";
+  static const newActivity = "/activity/new";
 }
