9e567f6008872c825d218dda21e3d0e9401800f0
gunblasterXTO <naufal.abdila@outlook.com>
2024-04-24 11:48:15 +0700

    feat: generalize border style drop down button

---
+++ lib/common/style/button.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import 'ui.dart';
+
+final InputDecoration dropDownButtonDecoration = InputDecoration(
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.circular(UIConst.standardRad),
+  ),
+  contentPadding: EdgeInsets.symmetric(
+    horizontal: UIConst.standardPadding * 2,
+  ),
+);
