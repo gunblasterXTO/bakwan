cbcd8de2f93e3a5b570967d52d119acfa31d6306
gunblasterXTO <naufal.abdila@outlook.com>
2024-05-08 10:11:38 +0700

    feat: change approach custom time picker

--- lib/modules/journal/widgets/form.dart
+++ lib/modules/journal/widgets/form.dart
@@ -5,61 +5,76 @@ import 'package:get/get.dart';
 class CustomTimePicker extends StatelessWidget {
   final String label;
   final String hint;
+  final TextEditingController controller;
+  final String? Function(String?)? validator;
   final RxString state;
-  final String Function(TimeOfDay) onChange;
-  final double? width;
+  final String Function(TimeOfDay) parser;
+  final double width;
 
   const CustomTimePicker({
     super.key,
     required this.label,
     required this.hint,
+    required this.controller,
     required this.state,
-    required this.onChange,
-    this.width,
+    required this.parser,
+    required this.width,
+    this.validator,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          label,
-          style: context.textTheme.bodySmall,
-        ),
-        SizedBox(
-          height: UIConst.standardGapHeight,
-        ),
-        GestureDetector(
-          child: Container(
-            width: width ?? 150,
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.black.withOpacity(0.7)),
-              borderRadius: BorderRadius.circular(UIConst.standardRad),
-            ),
-            padding: EdgeInsets.all(UIConst.standardPadding * 2),
-            child: Obx(() {
-              return Text(
-                state.value.isNotEmpty ? state.value : hint,
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: state.value.isNotEmpty ? Colors.black : Colors.grey,
-                ),
-              );
-            }),
+    return SizedBox(
+      width: width,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            label,
+            style: context.textTheme.bodySmall,
           ),
-          onTap: () async {
-            TimeOfDay? selectedTime = await showTimePicker(
-              context: context,
-              initialTime: TimeOfDay.now(),
-              initialEntryMode: TimePickerEntryMode.inputOnly,
-            );
-            if (selectedTime != null) {
-              state.value = onChange(selectedTime);
-            }
-          },
-        ),
-      ],
+          SizedBox(
+            height: UIConst.standardGapHeight,
+          ),
+          TextFormField(
+            controller: controller,
+            validator: validator,
+            readOnly: true,
+            onTap: () async {
+              TimeOfDay? selectedTime = await showTimePicker(
+                context: context,
+                initialTime: TimeOfDay.now(),
+                initialEntryMode: TimePickerEntryMode.inputOnly,
+                builder: (BuildContext context, Widget? child) {
+                  return MediaQuery(
+                    data: MediaQuery.of(context)
+                        .copyWith(alwaysUse24HourFormat: true),
+                    child: child ?? Container(),
+                  );
+                },
+              );
+              if (selectedTime != null) {
+                String parsedTime = parser(selectedTime);
+                controller.text = parsedTime;
+                state.value = parsedTime;
+              }
+            },
+            textAlign: TextAlign.center,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(UIConst.standardRad),
+                borderSide: const BorderSide(color: Colors.black),
+              ),
+              hintText: hint,
+              hintStyle: context.textTheme.bodyMedium!
+                  .copyWith(color: UIConst.hintTextColor),
+            ),
+            style: context.textTheme.bodyMedium!.copyWith(
+              color: UIConst.formValueTextColor,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
