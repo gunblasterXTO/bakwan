688de4ea4d8639f655088a4583cb527502a6d0c6
gunblasterXTO <naufal.abdila@outlook.com>
2024-05-08 13:54:00 +0700

    feat: setup page transition

--- lib/routes/pages.dart
+++ lib/routes/pages.dart
@@ -1,3 +1,4 @@
+import 'package:bakwan/common/style/ui.dart';
 import 'package:bakwan/modules/journal/views/activity_form.dart';
 import 'package:bakwan/modules/journal/views/journal.dart';
 import 'package:bakwan/modules/splash/views/splash.dart';
@@ -12,7 +13,21 @@ class AppPages {
 
   static final routes = [
     GetPage(name: Routes.index, page: () => const Splashscreen()),
-    GetPage(name: Routes.journal, page: () => const JournalPage()),
-    GetPage(name: Routes.activity, page: () => const ActivityFormPage()),
+    GetPage(
+      name: Routes.journal,
+      page: () => const JournalPage(),
+      transition: Transition.fadeIn,
+      transitionDuration: Duration(
+        milliseconds: UIConst.pageTransitionDuration,
+      ),
+    ),
+    GetPage(
+      name: Routes.activity,
+      page: () => const ActivityFormPage(),
+      transition: Transition.rightToLeft,
+      transitionDuration: Duration(
+        milliseconds: UIConst.pageTransitionDuration,
+      ),
+    ),
   ];
 }
